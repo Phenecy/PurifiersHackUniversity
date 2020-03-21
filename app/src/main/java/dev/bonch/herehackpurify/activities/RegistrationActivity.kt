cdc722702835c12d933a8e2e10db42e021b1c1aa
phenecy <phenecy.dev@gmail.com>
2020-03-21 20:52:01 +0300

    feat(layout): RegistrationActivity done

---
+++ app/src/main/java/dev/bonch/herehackpurify/activities/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package dev.bonch.herehackpurify.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.bonch.herehackpurify.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
