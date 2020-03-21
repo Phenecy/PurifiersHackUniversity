5364e5e1fb10ed00b8176b781b9027900a1a4a07
phenecy <phenecy.dev@gmail.com>
2020-03-21 17:44:04 +0300

    feat(layout): auth screen added

---
+++ app/src/main/java/dev/bonch/herehackpurify/AuthActivity.kt
@@ -0,0 +1,11 @@
+package dev.bonch.herehackpurify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
