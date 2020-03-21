77d555cf8279f272719ff6f7f3c00c92716b6022
phenecy <phenecy.dev@gmail.com>
2020-03-21 19:13:20 +0300

    fix(layout): auth screen done

--- app/src/main/java/dev/bonch/herehackpurify/SplashActivity.kt
+++ app/src/main/java/dev/bonch/herehackpurify/SplashActivity.kt
@@ -1,19 +1,28 @@
 package dev.bonch.herehackpurify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.os.HandlerCompat.postDelayed
+import kotlinx.android.synthetic.main.auth_afterload.*
 
 class SplashActivity : AppCompatActivity() {
 
+    //Splash views
     private lateinit var splashLogo: ImageView
     private lateinit var animation: Animation
     private lateinit var splashLayout: ConstraintLayout
+
+    //Auth views
+    private lateinit var phoneEditText: EditText
+    private lateinit var smsSendButton: Button
 
     private val SPLASH_DURATION = 500
 
@@ -40,9 +49,26 @@ class SplashActivity : AppCompatActivity() {
                 override fun onAnimationRepeat(animation: Animation) {}
 
                 override fun onAnimationEnd(animation: Animation) {
-
+//                    auth = ...
+//                    val currentUser = auth.
+//                    updateUI(currentUser)
+                    updateUI()
                 }
             })
         }, SPLASH_DURATION.toLong())
+    }
+
+    //    private fun updateUI(user: User?)
+    private fun updateUI() {
+//        if (user != null) {
+//            intent = Intent(this@SplashActivity, MainActivity::class.java)
+//            intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
+//            startActivity(intent)
+//            finish()
+//        }
+        setContentView(R.layout.auth_afterload)
+
+        phoneEditText = findViewById(R.id.phone_number_et)
+        smsSendButton = findViewById(R.id.sms_send_btn)
     }
 }
